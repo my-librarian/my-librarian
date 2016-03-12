559d0d504422450bd4d8cd9fe879711c3146b650
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-03-12 23:50:11 +0530

    Add header part for the website

---
+++ include/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Librarian</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <header>
+      <div class="content">
+        <div class="left">
+          <h1 class="title">My Librarian</h1>
+          <p>
+            Department of Sanskrit - RKMVC
+          </p>
+        </div>
+        <nav class="right">
+          <ul>
+            <li><a href="login.php">Log In</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main>
