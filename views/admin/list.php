0ab3bd72488ec8565e02cd5ebc2a33be2c9402af
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-03-13 23:56:41 +0530

    Add admin list page

---
+++ views/admin/list.php
@@ -0,0 +1,14 @@
+<table class="books list">
+  <tr>
+    <th class="accessno">
+      AccnNo
+    </th>
+    <th class="title">
+      Title
+    </th>
+    <th class="actions">
+      Actions
+    </th>
+  </tr>
+</table>
+<script src="/js/admin/list.js"></script>
