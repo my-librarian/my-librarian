1b49f522e944d5d25de26d38d108c937fee6fe05
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-03-13 23:49:09 +0530

    Get books from database

---
+++ app_data/config.php
@@ -0,0 +1,15 @@
+<?php
+
+  $db_host = 'localhost';
+  $db_name = 'my_librarian';
+  $db_user = 'root';
+  $db_pass = '222585';
+
+  $link = mysqli_connect(
+    $db_host,
+    $db_user,
+    $db_pass,
+    $db_name
+  );
+
+?>
