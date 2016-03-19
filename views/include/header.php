016e0bed68aaa7f8ee41074e8ae875bd3f3be15b
Gopikrishna Sathyamurhy <sandalblack19@gmail.com>
2016-03-19 14:35:29 -0500

    Include jQuery UI  Point to correct admin page

--- views/include/header.php
+++ views/include/header.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>My Librarian</title>
+    <link rel="stylesheet" href="/css/jquery-ui.css">
+    <link rel="stylesheet" href="/css/jquery-ui.structure.css">
+    <link rel="stylesheet" href="/css/jquery-ui.theme.css">
     <link rel="stylesheet" href="/css/master.css">
     <script src="/js/jquery.js"></script>
+    <script src="/js/jquery-ui.js"></script>
   </head>
   <body>
     <header>
@@ -17,7 +21,7 @@
         </a>
         <nav class="right">
           <ul>
-            <li><a href="/admin/list">Log In</a></li>
+            <li><a href="/admin/books">Log In</a></li>
           </ul>
         </nav>
       </div>
