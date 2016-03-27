ad52137c86db350ade4f834712f89eab35153d91
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-03-27 02:28:11 -0500

    Handle book edit

--- js/admin/books/edit-form-manager.js
+++ js/admin/books/edit-form-manager.js
@@ -1,19 +1,80 @@
 var editFormManager = {
 
-  edit: function (book) {
+  form: $('section.edit .book'),
+  book: null,
 
-    var form = $('.edit .book');
+  bindCancelButton: function () {
+
+    $('.cancel', editFormManager.form).click(editFormManager.cancel);
+  },
+
+  bindSaveButton: function () {
+
+    $('.save', editFormManager.form).click(editFormManager.saveBook);
+  },
+
+  cancel: function () {
+
+    if (editFormManager.book) {
+      listManager.books.push(editFormManager.book);
+      editFormManager.book = null;
+    }
+
+    editFormManager.form[0].reset();
+    $('section.edit').removeClass('active');
+    $('section.add').removeClass('edit-mode');
+    $('.txt', editFormManager.form).removeClass('error');
+    errorManager.removeAllErrors();
+  },
+
+  cancelFormSubmit: function () {
+
+    editFormManager.form.submit(false);
+  },
+
+  edit: function (book) {
 
     console.log(book);
 
-    $('.txt.accessno', form).val(book.accessno);
-    $('.txt.rackno', form).val(book.rackno);
-    $('.txt.adddate', form).val(book.adddate);
-    $('.txt.title', form).val(book.title);
-    $('.txt.subject', form).val(book.subject);
-    $('.txt.author', form).val(book.author);
+    editFormManager.cancel();
+    editFormManager.book = book;
 
-    $('.edit').addClass('active');
+    listManager.books = listManager.books.filter(function (lmBook) {
+      return book.id != lmBook.id;
+    });
+
+    $('.txt.accessno', editFormManager.form).val(book.accessno);
+    $('.txt.rackno', editFormManager.form).val(book.rackno);
+    $('.txt.adddate', editFormManager.form).val(book.adddate);
+    $('.txt.title', editFormManager.form).val(book.title);
+    $('.txt.subject', editFormManager.form).val(book.subject);
+    $('.txt.author', editFormManager.form).val(book.author);
+
+    $('section.edit').addClass('active');
+    $('section.add').addClass('edit-mode');
+  },
+
+  saveBook: function () {
+
+    var book = formManager.validateBook(editFormManager.form);
+
+    if (book) {
+
+      book.id = editFormManager.book.id;
+
+      $.ajax({
+        url: '/app_data/books/update.php',
+        method: 'post',
+        data: book,
+        success: function () {
+
+          var bookNode = $('#book-' + book.id);
+          $('.accessno', bookNode).html(book.accessno);
+          $('.title', bookNode).html(book.title);
+          editFormManager.cancel();
+        }
+      })
+    }
 
   }
 
@@ -21,8 +82,8 @@ var editFormManager = {
 
 $(function () {
 
-  var form = $('.edit .book');
-
-  form.submit(false);
+  editFormManager.cancelFormSubmit();
+  editFormManager.bindCancelButton();
+  editFormManager.bindSaveButton();
 
 });
