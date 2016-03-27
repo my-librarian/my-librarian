e7ad6b956089f9af6f70673c070b5e12bb284889
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-03-27 13:32:24 -0500

    Handle edit author

---
+++ app_data/authors/update.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../config.php';
+
+$data = [];
+
+foreach($_POST as $key => $value) {
+  $data[$key] = mysqli_escape_string($link, $value);
+}
+
+$authorId = $data['id'];
+$name = $data['name'];
+
+$fields = implode(', ', $fields);
+$query = "UPDATE author SET `name` = '$name' WHERE id = '$authorId'";
+mysqli_query($link, $query) or die(mysqli_error($link));
