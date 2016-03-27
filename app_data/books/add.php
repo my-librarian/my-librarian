2f13779dc6382abb14423fa6b7aade9db341b7d6
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-03-27 01:04:48 -0500

    Extract get id to separate file

--- app_data/books/add.php
+++ app_data/books/add.php
@@ -1,29 +1,12 @@
 <?php
 
 include '../config.php';
+include 'get-id.php';
 
 $data = [];
 
 foreach($_POST as $key => $value) {
   $data[$key] = mysqli_escape_string($link, $value);
-}
-
-function getId($table, $name) {
-
-  global $link;
-
-  $query = "SELECT id FROM $table WHERE name LIKE '$name'";
-  $result = mysqli_query($link, $query) or http_response_code(500);
-  $row = mysqli_fetch_assoc($result);
-
-  if($row) {
-    return $row['id'];
-  } else {
-    $query = "INSERT INTO $table(name) values('$name')";
-    mysqli_query($link, $query) or http_response_code(500);
-    return mysqli_insert_id($link);
-  }
-
 }
 
 $subjectId = getId('subject', $data['subject']);
