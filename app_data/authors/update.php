65df4d79e8b9ae2deb226147f2ed324f96f5bc78
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-03-27 13:52:49 -0500

    Close php tag

--- app_data/authors/update.php
+++ app_data/authors/update.php
@@ -11,6 +11,7 @@ foreach($_POST as $key => $value) {
 $authorId = $data['id'];
 $name = $data['name'];
 
-$fields = implode(', ', $fields);
 $query = "UPDATE author SET `name` = '$name' WHERE id = '$authorId'";
 mysqli_query($link, $query) or die(mysqli_error($link));
+
+?>
